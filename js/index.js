ed6c15a378b4ed41d0d5a4ab744a12db86717eb0
Filippa <filippabellange@live.se>
2021-11-02 19:02:55 +0100

    sass

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
+window.onload = function() {
+    start();
+}
+
+
+function start() {
 let n =  new Date();
 let year = n.getFullYear();
 let month = n.getMonth() + 1;
 let date = n.getDate();
 document.getElementById("date").innerHTML = month + "/" + date + "/" + year;
+}
 
