6176ed831f4cbd8b9d6536c549a1682b70c41c1c
Filippa <filippabellange@live.se>
2021-11-04 14:12:52 +0100

    list loop

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 window.onload = function() {
     start();
     document.getElementById("buttonInput").addEventListener('click', addTask)
-    // document.getElementById("inputValue").addEventListener('click', clearInput)
+    document.getElementById("inputValue").addEventListener('click', clearInput)
     document.getElementById("list-item").addEventListener('click', moveTask)
 
 }
@@ -16,15 +16,17 @@ document.getElementById("date").innerHTML = month + "/" + date + "/" + year;
 }
 
 function clearInput(){
-  document.getElementById("inputValue").value = " ";
+  document.getElementById("inputValue").value = "";
 }
 
 let taskList = [];
 function addTask(){
     let li = document.createElement("li");
     li.id = "list-item";
+
     let newTask = document.getElementById("inputValue").value;
     let task = document.createTextNode(newTask);
+  
     li.appendChild(task);
 
 
@@ -33,14 +35,18 @@ function addTask(){
     } else {
       document.getElementById("list-ul").appendChild(li);
       taskList.push(task);
-      console.log(taskList);
+
     }
 } 
 
 function moveTask () {
 
   for (let i = 0; i < taskList.length; i++) {
-    taskList
+
+    
+    // taskList[i].addEventListener("click", ()=> {taskList.splice([i], 1)})
+    taskList[i].addEventListener("click", ()=> {taskList[i] = [0]})
+    console.log(taskList);
     
   }
 }
