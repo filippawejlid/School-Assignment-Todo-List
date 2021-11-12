8f79ccc201ab4fc807c65a3f655e0e084dfbadda
Filippa <filippabellange@live.se>
2021-11-12 13:50:33 +0100

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,12 @@
 var gulp = require("gulp");
 var cssnano = require('gulp-cssnano');
 var sass = require("gulp-sass");
-const { on } = require("npmlog");
 
 gulp.task('sass', function(){
     return gulp.src('scss/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('css'))
 });
-// gulp.task("watch-scss", function () {
-    // gulp.watch("scss/**/*.scss", gulp.series("sass"));
-// });
-
 
 
 gulp.task('minify', function(){
